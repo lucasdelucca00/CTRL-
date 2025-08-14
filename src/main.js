f591dd0616430d948e9ddff20c69801d192aad19
lucasdelucca00 <lucaslucca@alunos.utfpr.edu.br>
2025-08-14 02:18:12 -0300

    att 01
    Header + banner + link direto + subfooter + footer

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { router } from './router.js';
 import './Header/style.css';
 import './HomePage/style.css';
+import './Footer/style.css';
 
 document.addEventListener('DOMContentLoaded', () => {
   router();
--- src/router.js
+++ src/router.js
@@ -16,6 +16,12 @@ export async function router() {
 
   const headerHtml = await fetch('/src/header/header.html').then(res => res.text());
   document.getElementById('header').innerHTML = headerHtml;
+
+  const homepageHtml = await fetch('/src/homepage/homepage.html').then(res => res.text());
+  document.getElementById('homepage').innerHTML = homepageHtml;
+
+  const footerHtml = await fetch('/src/footer/footer.html').then(res => res.text());
+  document.getElementById('footer').innerHTML = footerHtml;
 }
 
 window.addEventListener('hashchange', router);
